e73accffb16fa6f3e7697abe182b40efd4c3fe5d
saphalkoirala19 <koiralasaphal2@gmail.com>
2025-03-26 20:22:12 +0545

    First commit

---
+++ CricNagar/shop_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils.text import slugify
+
+class Product(models.Model):
+    CATEGORY = [("Bats", "BATS"), 
+                ("Balls", "BALLS"), 
+                ("Pads", "PADS")]
+
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(blank=True, null=True)
+    image = models.ImageField(upload_to="img")
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=15, choices=CATEGORY, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            unique_slug = base_slug
+            counter = 1
+            while Product.objects.filter(slug=unique_slug).exists():
+                unique_slug = f'{base_slug}-{counter}'
+                counter += 1
+            self.slug = unique_slug
+        super().save(*args, **kwargs)
